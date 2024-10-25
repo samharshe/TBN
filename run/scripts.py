6494dba591333f33623fb908c68303683142c799
Sammy Harshe <samharshe29@gmail.com>
2024-10-25 09:59:12 -0500

    refactored scripts.py

--- run/scripts.py
+++ run/scripts.py
@@ -1,43 +1,76 @@
 import torch, sys, functools, wandb
 from collections import defaultdict
 from torch import nn
-from torch.nn import Module
-from torch.utils.data import DataLoader
-from torch.optim import Optimizer
-from torch.optim.lr_scheduler import LRScheduler
+from torch.utils import data as torch_data
+from torch import optim
+from torch.optim import lr_scheduler
 from typing import Callable
 import matplotlib.pyplot as plt
 
-def train_test(model: Module,
-               optimizer: Optimizer,
-               loss_fn: Callable,
-               scheduler: LRScheduler,
-               train_dataloader: DataLoader,
-               val_dataloader: DataLoader,
-               test_dataloader: DataLoader,
-               cfg_dict: dict) -> None:
+def train_test(model: nn.Module, optimizer: optim.Optimizer, loss_fn: Callable, scheduler: lr_scheduler.LRScheduler, train_dataloader: torch_data.DataLoader, val_dataloader: torch_data.DataLoader, test_dataloader: torch_data.DataLoader, cfg_dict: dict) -> None:
+    """trains model on dataloader, saves weights of the best-performing model, and logs ongoing results through wandb.
     
+    parameters
+    ----------
+    model: Module
+        self-explanatory.
+    optimizer: Optimizer
+        self-explanatory.
+    loss_fn: Callable
+        self-explanatory.
+    scheduler: LRScheduler
+        self-explanatory.
+    train_dataloader: DataLoader
+        self-explanatory.
+    val_dataloader: DataLoader
+        self-explanatory.
+    test_dataloader: DataLoader
+        self-explanatory.
+    cfg_dict: dict
+        logged to wandb. used to set n_epochs, name, and y_version.
+
+    returns
+    -------
+    None
+    """
+    # initialize variables via cfg_dict
     n_epochs = cfg_dict['n_epochs']
-    name = cfg_dict['name']
-    y_version = cfg_dict['y_version']
+    name = cfg_dict['name'] # used to save weights
+    y_version = cfg_dict['y_version'] # used to determine which test function to use
     
-    wandb.init(project='TBN')
+    # initialize wandb
+    wandb.init(project='TBN') # online training
+    # wandb.init(project='TBN' mode='offline') # offline training
     wandb.config.update(cfg_dict)
     wandb.watch(model, log="all", log_freq=50, log_graph=True)
+
+    # train and test model
     try:
+        # train model
         train(model=model, optimizer=optimizer, scheduler=scheduler, loss_fn=loss_fn, train_dataloader=train_dataloader, val_dataloader=val_dataloader, n_epochs=n_epochs, name=name)
+        # test model depending on y_version
         if y_version == 'score':
             test_score(model=model, loss_fn=loss_fn, test_dataloader=test_dataloader)
         elif y_version == 'home_win':
             test_classifier(model=model, loss_fn=loss_fn, test_dataloader=test_dataloader)
+        # perhaps other test functions to be added later
         else:
             raise ValueError(f'y_version must be either "score" or "home_win". passed value: {y_version}.')
     finally:
         wandb.finish()
 
-def test_classifier(model: Module, 
-          loss_fn: Callable, 
-          test_dataloader: DataLoader) -> None:
+def test_classifier(model: nn.Module, loss_fn: Callable,  test_dataloader: torch_data.DataLoader) -> None:
+    """tests model that predicts binary outcomes.
+
+    parameters
+    ----------
+    model: Module
+        self-explanatory.
+    loss_fn: Callable
+        self-explanatory.
+    test_dataloader: DataLoader
+        self-explanatory.
+    """
     # track training losses
     test_losses = []
     # track correct predictions
@@ -51,7 +84,7 @@ def test_classifier(model: Module,
         # predictions from the model
         y_hat = model(x)
 
-        # keep track of 
+        # keep track of correct predictions
         n_correct += torch.sum(torch.sigmoid(y_hat).round() == y).item()
         n_total += len(y)
         
@@ -69,12 +102,10 @@ def test_classifier(model: Module,
     print(f'TEST MEAN LOSS: {test_mean_loss:04f}')
     print(f'TEST ACCURACY: {test_accuracy:04f}')
     
-    # Log to wandb
+    # log to wandb
     wandb.log({"test_mean_loss": test_mean_loss, "test_accuracy": test_accuracy})
 
-def test_score(model: Module, 
-          loss_fn: Callable, 
-          test_dataloader: DataLoader) -> None:
+def test_score(model: nn.Module, loss_fn: Callable, test_dataloader: torch_data.DataLoader) -> None:
     # track training losses
     test_losses = []
     # track correct predictions
@@ -116,14 +147,7 @@ def test_score(model: Module,
     # Log to wandb
     wandb.log({"test_mean_loss": test_mean_loss, "test_accuracy": test_accuracy})
 
-def train(model: Module, 
-          optimizer: Optimizer, 
-          scheduler: LRScheduler, 
-          loss_fn: Callable, 
-          train_dataloader: DataLoader, 
-          val_dataloader: DataLoader, 
-          n_epochs: int, 
-          name: str) -> None:
+def train(model: nn.Module, optimizer: optim.Optimizer, scheduler: lr_scheduler.LRScheduler, loss_fn: Callable, train_dataloader: torch_data.DataLoader, val_dataloader: torch_data.DataLoader, n_epochs: int, name: str) -> None:
     """trains model on dataloader, saves weights of the best-performing model, and logs ongoing results through wandb.
     
     parameters
@@ -144,6 +168,10 @@ def train(model: Module,
         self-explanatory.
     name: str
         best-performing weights are saved to f'/src/weights/{name}.pth'.
+
+    returns
+    -------
+    None; logs results to wandb and prints epoch-by-epoch mean train and val losses.
     """
     # keep track of the best val performance to know when to save weights
     epoch_val_min_loss = sys.float_info.max
@@ -181,6 +209,7 @@ def train(model: Module,
             # keep track of training losses
             train_epoch_losses.append(loss.item())
             
+        # calculate and log mean losses from this epoch
         epoch_mean_train_loss = torch.mean(torch.tensor(train_epoch_losses)).item()
         wandb.log({"train_mean_loss": epoch_mean_train_loss})
             
@@ -210,7 +239,7 @@ def train(model: Module,
         # print out results of epoch
         print(f'EPOCH {epoch+1} OF {n_epochs} | {name} | TRAIN MEAN LOSS: {epoch_mean_train_loss:04f} | VAL MEAN LOSS: {epoch_mean_val_loss:04f}')
         
-        # Log to wandb
+        # log to wandb
         wandb.log({
             "val_mean_loss": epoch_mean_val_loss,
             "learning_rate": optimizer.param_groups[0]['lr']
@@ -235,22 +264,38 @@ def train(model: Module,
     plt.title('training loss')
     plt.show()
     
-def check_memory(model: Module, dataloader: DataLoader, optimizer: Optimizer=torch.optim.SGD, loss_fn: Callable=torch.nn.MSELoss, n_epochs: int=100, batch_size: int=4, lr: float=1e-3) -> None:
+def check_memory(model: nn.Module, dataloader: torch_data.DataLoader, optimizer: optim.Optimizer = optim.SGD, loss_fn: Callable = nn.MSELoss, n_epochs: int = 100, batch_size: int = 4, lr: float = 1e-3) -> None:
+    """checks memory usage of model.
+    
+    parameters
+    ----------
+    model: Module
+        self-explanatory.
+    dataloader: DataLoader
+        self-explanatory.
+
+    returns
+    -------
+    None; prints final prediction, actual value, and final loss and makes matplotlib plot of training losses.
+    """
+    try:
+        # make single mini batch
+        x, y = next(iter(dataloader))
+        x = x[0:batch_size]
+        y = y[0:batch_size]
+    except ValueError:
+        raise ValueError(f'batch_size passed into this function must be an integer less than or equal to the batch size of the dataloader passed into this function. batch_size passed into this function: {batch_size}. batch size of dataloader passed into this function: {next(iter(dataloader))[0].shape[0]}.')
+
     # initialize optimizer and loss function
     optimizer = optimizer(model.parameters(), lr=lr)
     loss_fn = loss_fn()
-        
-    # make single mini batch
-    x, y = next(iter(dataloader))
-    x = x[0:batch_size]
-    y = y[0:batch_size]
     
     # track gradients
     model.train()
     # track training losses
     train_losses = []
     # training loop occurs num_epochs times
-    for epoch in range(n_epochs):
+    for _ in range(n_epochs):
         # clear gradients
         optimizer.zero_grad()
         
@@ -269,7 +314,7 @@ def check_memory(model: Module, dataloader: DataLoader, optimizer: Optimizer=tor
         # keep track of training losses
         train_losses.append(loss.item())
         
-    # write gradient and weights and biases stats
+    # write gradient and weights and biases stats to memorize/
     write_stats(model)
     
     # print final prediction
@@ -286,7 +331,18 @@ def check_memory(model: Module, dataloader: DataLoader, optimizer: Optimizer=tor
     plt.title('training loss')
     plt.show()
     
-def write_stats(model: Module) -> None:
+def write_stats(model: nn.Module) -> None:
+    """writes gradient stats to memorize/gradients.txt and weight and bias stats to memorize/weights_and_biases.txt.
+    
+    parameters
+    ----------
+    model: Module
+        self-explanatory.
+    
+    returns
+    -------
+    None; modifies memorize/gradients.txt and memorize/weights_and_biases.txt.
+    """
     # write gradient stats
     with open('memorize/gradients.txt', 'w') as f:
         f.write(f"total number of model parameters: {sum(p.numel() for p in model.parameters())}\n" + '-'*60)
