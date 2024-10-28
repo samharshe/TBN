79c331cf84b802a5be24f812ba208eb89537ce14
Sammy Harshe <samharshe29@gmail.com>
2024-10-28 00:48:59 -0500

    you guys should try out linting its fun

--- src/process/preliminary_model.py
+++ src/process/preliminary_model.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 def simple_model_test(df, stat='ORB'):
     """
